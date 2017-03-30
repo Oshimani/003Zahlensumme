5665f87c68e747b77fe5002a77b0928139bf02fd
Jannick Jungbluth <jannick2.0@googlemail.com>
2017-03-30 19:25:12 +0200

    Init

---
+++ 003Zahlensumme/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003Zahlensumme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summe = 0;
+            int startZahl =Convert.ToInt16(Console.ReadLine());
+            int stopZahl = Convert.ToInt16(Console.ReadLine());
+
+            while (startZahl <= stopZahl)
+            {
+                summe = summe + startZahl;
+                startZahl = startZahl + 1;
+            }
+            Console.WriteLine("Summe: {0}", summe);
+        }
+    }
+}
